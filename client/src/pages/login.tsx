848b6c7e0835e12b548f0cfa289f4dea82276552
EvanKim <evan977512@gmail.com>
2023-07-11 17:43:06 -0600

    login ui created

---
+++ client/src/pages/login.tsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { FormEvent, useState } from "react";
+import InputGroup from "../components/inputGroup";
+
+const Login = () => {
+  return;
+  <div className="bg-white">
+    <div className="flex flex-col items-center justify-center h-screen p-6">
+      <div className="w-10/12 mx-auto md:w-96">
+        <h1 className="mb-2 text-lg font-medium">Login</h1>
+        <form onSubmit={handleSubmit}>
+          <InputGroup placeholder="Username" value={username} setValue={setUsername} error={errors.username} />
+          <InputGroup placeholder="Password" value={password} setValue={setPassword} error={errors.password} />
+          <button className="w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded">
+            Login
+          </button>
+        </form>
+        <small>
+          haven't registered yet?
+          <Link href="/register" legacyBehavior>
+            <a className="ml-1 text-blue-500 uppercase">register</a>
+          </Link>
+        </small>
+      </div>
+    </div>
+  </div>;
+};
+
+export default Login;
