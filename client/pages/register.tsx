111a2fc4b1f6eb4a2502e04da74dda9658e5fe66
EvanKim <evan977512@gmail.com>
2023-07-11 14:00:28 -0600

    now it is working yay

---
+++ client/pages/register.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const register = () => {
+  return (
+    <div className="bg-white">
+      <div className="flex flex-col items-center justify-center h-screen p-6">
+        <div className="w-10/12 mx-auto md:w-96">
+          <h1 className="mb-2 text-lg font-medium">register</h1>
+          <form></form>
+          <button className="w-full py-2 mb-1 text-xs font-bold text-white uppercate bg-grey-400 border border-gray-400 "></button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default register;
