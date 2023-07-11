7b39a61e9e20448495c59f742646746cec48a1e2
EvanKim <evan977512@gmail.com>
2023-07-11 15:29:23 -0600

    database register connected

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { FormEvent, useState } from "react";
 import InputGroup from "../components/inputGroup";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 const Register = () => {
   const [email, setEmail] = useState("");
@@ -8,12 +10,30 @@ const Register = () => {
   const [password, setPassowrd] = useState("");
   const [errors, setErrors] = useState<any>({});
 
+  let router = useRouter();
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/auth/register", {
+        email,
+        username,
+        password,
+      });
+      console.log("res", res);
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error);
+      setErrors(error.response.data || {});
+    }
+  };
+
   return (
     <div className="bg-white">
       <div className="flex flex-col items-center justify-center h-screen p-6">
         <div className="w-10/12 mx-auto md:w-96">
           <h1 className="mb-2 text-lg font-medium">register</h1>
-          <form>
+          <form onSubmit={handleSubmit}>
             <InputGroup placeholder="Email" value={email} setValue={setEmail} error={errors.email} />
             <InputGroup placeholder="UserName" value={username} setValue={setUsername} error={errors.username} />
             <InputGroup placeholder="Password" value={password} setValue={setPassowrd} error={errors.password} />
